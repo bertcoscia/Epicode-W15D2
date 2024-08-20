2e035b2db09e113dc08f2c6b9546f20a862311d4
A. Alberto Coscia <angeloalberto.coscia@gmail.com>
2024-08-20 15:11:32 +0200

    connects java to db

--- src/main/java/a/albertocoscia/Application.java
+++ src/main/java/a/albertocoscia/Application.java
@@ -1,6 +1,11 @@
 package a.albertocoscia;
 
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
 public class Application {
+
+    private static final EntityManagerFactory emf = Persistence.createEntityManagerFactory("gestione_eventi");
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
